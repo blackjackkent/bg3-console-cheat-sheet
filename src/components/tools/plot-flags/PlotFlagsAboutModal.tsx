46b03feb143c90cb16c0994a308ad0f5ea4d1682
Rosalind W <blackjacksoftwaredev@gmail.com>
2025-05-23 17:29:13 -0500

    Plot flags about

--- src/components/tools/plot-flags/PlotFlagsAboutModal.tsx
+++ src/components/tools/plot-flags/PlotFlagsAboutModal.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Code, Text } from "@chakra-ui/react";
 import CommandText from "../../common/CommandText";
 import AboutModal from "../../common/AboutModal";
 
@@ -7,13 +7,35 @@ const PlotFlagsAboutModal = () => {
 		<AboutModal title="About Plot Flags">
 			<Text mb={4}>
 				Plot flags are used to indicate whether or not a particular circumstance
-				is true in the current state of the active save.
+				is true in the current state of the active save. Among other things,
+				they are used to determine the route taken through the branching paths
+				of a dialogue tree.
 			</Text>
-			<Text>
-				To check if a particular flag is currently set, use the following
-				command (&quot;0&quot; means it is unset, &quot;1&quot; means it is
-				set):
+			<Text mb={4}>
+				When checking the state of a flag, <Code>0</Code> means that it is unset
+				(false), and <Code>1</Code> means that it is set (true).
 			</Text>
+
+			<Text mb={4}>
+				Flags have a name and a UUID (universally unique identifier). The UUID
+				is the important part when being passed to commands, but the name can be
+				attached for clarity.
+			</Text>
+			<Text> In other words:</Text>
+			<Code my={2} mx="auto" display="block" width="fit-content">
+				010d2d26-74f9-4e3d-8437-4c23694dd591
+			</Code>
+			<Text mx="auto" display="block" width="fit-content">
+				and
+			</Text>
+			<Code my={2} mx="auto" display="block" width="fit-content">
+				LOW_JaheirasHouse_Event_FoundKhalidsGift010d2d26-74f9-4e3d-8437-4c23694dd591
+			</Code>
+			<Text mb={4}>
+				are treated equivalently as UUID parameters to Osiris commands.
+			</Text>
+
+			<Text>To check if a particular flag is currently set:</Text>
 			<CommandText
 				value={`print(GetFlag("{flag UUID}", "{character UUID}"))`}
 			/>
